addaefbf5faaaa23f27f95883f6d59b04e862673
Victor 'Yussak' Degliame <victor.degliame@epita.fr>
2013-07-29 02:57:49 +0200

    Update: The search is working! just have to sort and format the output now. AMAZING

--- src/interpreter/interpreter.cpp
+++ src/interpreter/interpreter.cpp
@@ -1,6 +1,6 @@
 #include "interpreter.hpp"
 
-#define MIN(a, b)(a < b ? a : b)
+#define MAX(a, b)(a > b ? a : b)
 
 Interpreter::Interpreter(std::string file)
 {
@@ -10,14 +10,16 @@ Interpreter::Interpreter(std::string file)
     iar >> this->p;
 }
 
-stringVec Interpreter::getResults(int distance, std::string word)
+void Interpreter::getResults(unsigned short distance, std::string word)
 {
     maxDist = distance;
     this->word = word;
 
     browse(this->p->root);
 
-    return this->results;
+    for (auto& r : results)
+        std::cout << r.first << " Distance: " << r.second.first <<
+            " Frequency: " << r.second.second << std::endl;
 }
 
 void Interpreter::browse(Node* n)
@@ -28,15 +30,10 @@ void Interpreter::browse(Node* n)
         getNextWord(it->second, 0, 0, 0, "");
 }
 
-size_t myMin(size_t dist1, size_t dist2, size_t dist3)
-{
-    return MIN(dist1, MIN(dist2, dist3));
-}
-
 // Current node,i: index word in node, j: index in word searched, distance
-void Interpreter::getNextWord(Node* n, int i, int j, int dist, std::string curWord)
+void Interpreter::getNextWord(Node* n, unsigned short i, unsigned short j, unsigned short dist, std::string curWord)
 {
-    if (dist > maxDist || (int)(n->length + 1 - word.length()) > (int)maxDist)
+    if (dist > maxDist || (int)n->length + 1 - (int)word.length() > (int)maxDist)
         return;
 
     // Stop case
@@ -44,10 +41,21 @@ void Interpreter::getNextWord(Node* n, int i, int j, int dist, std::string curWo
     {
         std::string accWord("");
         accWord += n->c;
-        for (size_t a = 0; a < n->length; ++a)
+        for (unsigned short a = 0; a < n->length; ++a)
             accWord += p->suffixes[n->index + a];
-        if (n->isWord && dist + word.length() - j <= maxDist && dist + n->length - i + 1 <= maxDist)
-            std::cout << curWord + accWord << std::endl;
+        
+        unsigned short dist1 = dist + word.length() - j;
+        unsigned short dist2 = dist + n->length - i + 1;
+
+        if (n->isWord && dist1 <= maxDist && dist2 <= maxDist)
+        {
+            unsigned short myDist = MAX(dist1, dist2);
+            std::string myWord = curWord + accWord;
+            resMap::iterator it;
+
+            if ((it = results.find(myWord)) == results.end() || it->second.first > myDist)
+                results[myWord] = std::pair<unsigned short, size_t>(myDist, n->freq);
+        }
 
         if (i > n->length)
             for (Node::nodeMap::iterator it = n->sons.begin();
@@ -72,37 +80,4 @@ void Interpreter::getNextWord(Node* n, int i, int j, int dist, std::string curWo
     else
         getNextWord(n, i + 1, j + 1, dist, curWord); // equal letters
 }
-
-/*
-   size_t Interpreter::getNextWord(Node* n, unsigned short curIndex, unsigned short curDist, std::string& suf, int& i)
-   {
-   int dist = 0;
-   int insertDist = 0;
-   std::string sufString;
-   char c;
-
-   if (n->c != this->word[curIndex])
-   ++dist;
-
-   suf += n->c;
-   for (; (i < (int)n->length) && (curIndex + i < (int)this->word.length()) &&
-   (curDist + dist <= maxDist); ++i)
-   {
-   c = p->suffixes[n->index + i];
-   if (c != this->word[curIndex + i + 1])
-   ++dist;
-   suf += c;
-   }
-
-   for (size_t j = i; j < n->length; ++j)
-   suf += p->suffixes[n->index + j];
-
-   if (curDist + dist + n->length - i > maxDist)
-   return maxDist + 1;
-
-   return dist + n->length - i;
-   }
-
-*/
-
 
